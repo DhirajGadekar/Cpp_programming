afd275170904469fe0f66598405ee1f9730077ae
dhirajgadekar <dhirajgadekar629@gmail.com>
2023-05-14 12:18:02 +0530

    Structure and ClassesAndObject related Code

---
+++ ClassAndObject/Class Codes/1_Strucure.cpp
@@ -0,0 +1,31 @@
+//Structure In Cpp :
+
+#include<iostream>
+
+struct Player {
+	
+	int jerNo = 45;
+
+	std::string pName = "Rohit Sharma";
+
+	int playerInfo(int x) {
+		
+		std::cout << x << std::endl;
+		std::cout<< "Name :" << pName << std::endl;
+		std::cout<< "Jercy No :" << jerNo << std::endl;
+
+		return x + 10;
+	}
+};
+
+int main() {
+	
+	Player obj;
+	
+	std::cout<< obj.pName << std::endl;
+	std::cout<< obj.jerNo << std::endl;
+
+	int ret = obj.playerInfo(20);
+	std::cout << ret << std::endl;
+	return 0;
+}
---
+++ ClassAndObject/Class Codes/2_ClassAndObj.cpp
@@ -0,0 +1,25 @@
+
+#include<iostream>
+
+class Player {
+	
+	std::string pName = "Rohit Sharma";
+	int jerNo = 45;
+	float avg = 56.43f;
+
+	public :
+	void playerInfo() {
+		
+		std::cout << "Name : " << pName << std::endl;
+		std::cout << "Jercy NO : " << jerNo << std::endl;
+		std::cout << "Average : " << avg << std::endl;
+	}
+};
+
+int main() {
+	
+	Player obj;
+	obj.playerInfo();
+
+	return 0;
+}
