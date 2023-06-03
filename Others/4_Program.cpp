7890c75f1890d3bddacfe9b67c41886376f7b008
dhirajgadekar <dhirajgadekar629@gmail.com>
2023-06-03 23:15:30 +0530

    Updated Other Files

---
+++ Others/4_Program.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+int main() {
+	
+	int cout = 10;
+	std::cout<< cout << std::endl;
+
+	return 0;
+}
---
+++ Others/5_Program.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	
+	int cout = 10;
+	cout << cout << endl;
+	return 0;
+}
