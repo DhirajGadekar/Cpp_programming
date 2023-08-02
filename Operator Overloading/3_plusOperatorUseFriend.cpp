0fa51de539e7a16fabc90de95729bb608e512d65
dhirajgadekar <dhirajgadekar629@gmail.com>
2023-08-02 07:42:45 +0530

    Plus Operator Overloading

---
+++ Operator Overloading/3_plusOperatorUseFriend.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+class Demo {
+	
+	int x;
+	int y;
+	public:
+		Demo(int x, int y) {
+			
+			this->x = x;
+			this->y = y;
+		}
+		
+		friend int operator+(const Demo& obj1, const Demo& obj2);
+		friend int operator+(const Demo& obj1, int data) {
+			
+			return obj1.x + data;
+		}
+};
+
+int operator+(const Demo& obj1, const Demo& obj2) {
+	
+	return obj1.x + obj2.y;
+}
+
+int main() {
+	
+	Demo obj1(10,20);
+	Demo obj2(40,50);
+
+	std::cout << obj1 + obj2 << std::endl;
+	std::cout << obj1 + 100 << std::endl;
+
+	return 0;
+}
---
+++ Operator Overloading/5_plusOperatorUseMemberF.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+class OperatorOverload {
+	
+	int x;
+	public:
+		OperatorOverload(int x) {
+			
+			this->x = x;
+		}
+		int operator+(const OperatorOverload& obj) {
+			
+			return obj.x + this->x;
+		}
+};
+
+int main() {
+	
+	OperatorOverload obj1(10);
+	OperatorOverload obj2(20);
+
+	std::cout << obj1 + obj2 << std::endl;
+
+	return 0;
+}
