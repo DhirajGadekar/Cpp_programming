1e9a75043af48386158ddcf0c8d9cb5c6e973717
dhirajgadekar <dhirajgadekar629@gmail.com>
2023-08-02 08:13:40 +0530

    Arithmetic Operator : Operator Overloading

---
+++ Operator Overloading/9_ModuloOperatorUseFriend.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+class Demo {
+	
+	int x;
+	int y;
+	public:
+		Demo(int x, int y) {
+			
+			this->x = x;
+			this->y = y;
+		}
+		
+		friend int operator%(const Demo& obj1, const Demo& obj2);
+};
+
+int operator%(const Demo& obj1, const Demo& obj2) {
+	
+	return obj1.x % obj2.y;
+}
+
+int main() {
+	
+	Demo obj1(80,20);
+	Demo obj2(40,50);
+
+	std::cout << obj1 % obj2 << std::endl;
+	return 0;
+}
